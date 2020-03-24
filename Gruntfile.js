c0bf0306ce2b59187947e1b8229a7dffe29e1159
Tamara Bazko <tamarabazko@Tamaras-MBP.fritz.box>
2020-03-24 22:42:29 +0100

    Updating Portfolio, About and pics

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,6 @@ module.exports = function (grunt) {
 		pkg: grunt.file.readJSON('package.json'),
 		sass: {
 			options: {
-				sourcemap: 'none',
 				style: 'compressed'
 			},
 			dist: {
@@ -74,4 +73,5 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.registerTask('default', ['watch', 'assemble']);
+	grunt.registerTask('build', ['sass', 'assemble', 'autoprefixer', 'uglify']);
 }
